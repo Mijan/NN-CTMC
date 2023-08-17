31f975ef8d5ba00be2cbafa8a3128b1a1117898f
Jan Mikelson <jan.mikelson@gmail.com>
2023-08-17 09:30:18 +0200

    MLE estimate massively refacored and corrected plotting added

--- src/Estimation/MLE.py
+++ src/Estimation/MLE.py
@@ -1,86 +1,149 @@
 
 import numpy as np
-from typing import List
+from src.Models.models import Model
+import matplotlib.pyplot as plt
+from src.Utils.GeneralUtils import getRowsCols
 
 
-def computeMLETransitionMatrix(state_vector: List[np.array], time_vector: np.array) -> np.array:
-    # Convert state sequences to unique integers for efficient indexing
-    states, unique_indices = np.unique(np.vstack(state_vector), axis=0, return_inverse=True)
+class MLEstimator:
+    def __init__(self, model: Model, observation_vector: np.array, time_vector: np.array):
+        self.__observation_vector = observation_vector
+        self.__num_observations = observation_vector.shape[0]
 
-    n_states = len(states)
+        assert self.__num_observations == len(time_vector)
+        self.__time_vector = time_vector
 
-    # Initialize arrays for transition counts and state times
-    transition_counts = np.zeros((n_states, n_states))
-    state_times = np.zeros(n_states)
+        self.__model = model
+        self.__stoich = model.getStoichiometry()
+        self.__num_reactions = model.getNumReactions()
 
-    # Count transitions and compute state times
-    for idx in range(1, len(unique_indices)):
-        transition_counts[unique_indices[idx - 1], unique_indices[idx]] += 1
-        state_times[unique_indices[idx - 1]] += time_vector[idx] - time_vector[idx - 1]
+        self.__unique_states, self.__unique_indices = np.unique(observation_vector, axis=0, return_inverse=True)
+        self.__num_unique_states = self.__unique_states.shape[0]
 
-    # Calculate MLE transition rates
-    with np.errstate(divide='ignore', invalid='ignore'):
-        mle_transition_matrix = transition_counts / state_times[:, np.newaxis]
-    mle_transition_matrix[np.isnan(mle_transition_matrix)] = 0
+        self.__state_to_idx = {tuple(state): idx for idx, state in enumerate(self.__unique_states)}
 
-    return mle_transition_matrix
 
+    def computeMLETransitionMatrix(self) -> np.array:
+        # Convert state sequences to unique integers for efficient indexing
+
+        n_states = len(self.__unique_states)
+
+        # Initialize arrays for transition counts and state times
+        transition_counts = np.zeros((n_states, n_states))
+        state_times = np.zeros(n_states)
+
+        # Count transitions and compute state times
+        for idx in range(1, len(self.__unique_indices)):
+            transition_counts[self.__unique_indices[idx - 1], self.__unique_indices[idx]] += 1
+            state_times[self.__unique_indices[idx - 1]] += self.__time_vector[idx] - self.__time_vector[idx - 1]
+
+        # Calculate MLE transition rates
+        with np.errstate(divide='ignore', invalid='ignore'):
+            mle_transition_matrix = transition_counts / state_times[:, np.newaxis]
+        mle_transition_matrix[np.isnan(mle_transition_matrix)] = 0
+
+        return mle_transition_matrix
+
+    def plotMLEstimates(self, mle_transition_matrix: np.array):
+        estimated_propensities = self.__extractPropensitiesFromMLEMatrix(mle_transition_matrix)
+
+        true_propensities = np.zeros((self.__num_observations, self.__num_reactions))
+        for i, state in enumerate(self.__observation_vector):
+            true_propensities[i] = self.__model.getPropensities(state, self.__time_vector[i])
+
+        fig = self.__createPropensityPlot(estimated_propensities, true_propensities)
+        return fig
+
+    def __createPropensityPlot(self, estimated_propensities, true_propensities):
+        nrows, ncols = getRowsCols(self.__num_reactions)
+        # Visualization
+        fig, axs = plt.subplots(nrows, ncols, figsize=(5*ncols, 5*nrows))
+        for rct_nbr in range(self.__num_reactions):
+            if nrows >1:
+                ax = axs[rct_nbr // ncols, rct_nbr % ncols]  # Select the right subplot
+            else:
+                ax = axs[rct_nbr]
+
+            # Sum true propensities for reactions with the same stoichiometry
+            sum_true_propensities = true_propensities[:, rct_nbr]
+            for k, s in enumerate(self.__stoich):
+                if k != rct_nbr and np.array_equal(s, self.__stoich[rct_nbr]):
+                    sum_true_propensities += true_propensities[:, k]
+
+            ax.scatter(sum_true_propensities, estimated_propensities[:, rct_nbr], alpha=0.5)
+            ax.set_xlabel(f'True Propensities for Reaction {rct_nbr + 1}')
+            ax.set_ylabel(f'Estimated Propensities for Reaction {rct_nbr + 1}')
+
+            min_val = min(sum_true_propensities.min(), estimated_propensities[:, rct_nbr].min())
+            max_val = max(sum_true_propensities.max(), estimated_propensities[:, rct_nbr].max())
+
+            ax.plot([min_val, max_val], [min_val, max_val], color='red', linestyle='--')
+            ax.grid(True)
+            ax.set_title(f"Comparison for Reaction {rct_nbr + 1}")
+        plt.tight_layout()
+        return fig
+
+    def __extractPropensitiesFromMLEMatrix(self, mle_transition_matrix):
+        # Initialize the estimated propensities matrix
+        estimated_propensities = np.zeros((self.__num_observations, self.__num_reactions))
+
+        for obs_nbr, obs_state in enumerate(self.__observation_vector):
+            obs_state_idx = self.__state_to_idx[tuple(obs_state)]
+
+            # Iterate through the stoichiometry to determine possible target states
+            for reaction_idx, reaction_stoich in enumerate(self.__stoich):
+                target_state = obs_state + reaction_stoich
+                target_state_idx = self.__state_to_idx.get(tuple(target_state))
+
+                # If target_state_idx is not None, it means the target state exists in the MLE matrix
+                if target_state_idx is not None:
+                    estimated_propensities[obs_nbr, reaction_idx] = mle_transition_matrix[
+                        obs_state_idx, target_state_idx]
+
+        return estimated_propensities
+
+    def __createReactionMapping(self):
+
+        # Convert stoichiometry matrix rows to strings to make them hashable.
+        stoich_dict = {}
+        for rct_nbr, stoich_vect in enumerate(self.__stoich):
+            key = tuple(stoich_vect)
+            stoich_dict.setdefault(key, []).append(rct_nbr)
+
+        # Construct the reactions_mapping using only successive states
+        reactions_mapping = {}
+        for i in range(self.__num_observations - 1):  # -1 to avoid indexing out of range
+            source_state = self.__observation_vector[i]
+            target_state = self.__observation_vector[i + 1]
+
+            source_idx = self.__unique_indices[i]
+            target_idx = self.__unique_indices[i + 1]
+
+            difference = tuple(target_state - source_state)
+            matching_reactions = stoich_dict.get(difference, [])
+
+            if matching_reactions:
+                key = (source_idx, target_idx)
+                reactions_mapping[key] = matching_reactions[0] if len(matching_reactions) == 1 else tuple(
+                    matching_reactions)
+
+        return reactions_mapping
+
+
+# Debugging run
 if __name__ == '__main__':
-    from pathlib import Path
-    import numpy as np
-    from src.Models.models import ThreeSpeciesModel
-    from src.Estimation.MLE import computeMLETransitionMatrix
+    from src.Models.models import BirthDeath
     from src.Models.models import ThreeSpeciesModel
     from src.Simulator.SSA import SSASimulator
-    from src.Plotter.SystemPlotter import SysPlotter
-    import os
-
-    # data_folder = Path("../../data")
-    # data_name = "ThreeState.npz"
-    # print(os.getcwd())
-    # model = ThreeSpeciesModel()
-    # #
-    # data = np.load(str(data_folder / data_name))
-    # y = data['y']
-    # t = data['t']
-
+    from src.Estimation.MLE import MLEstimator
+    # model = BirthDeath()
     model = ThreeSpeciesModel()
     simulator = SSASimulator(model)
 
     parameters = model.getDefaultParameter()
-
-    y, t = simulator.run_ssa(np.array([80000, 10, 10]), 1000, parameters)
-    plotter = SysPlotter()
-    fig = plotter.plotSystem(y, t, model)
-
-    model = ThreeSpeciesModel()
-    simulator = SSASimulator(model)
-    computeMLETransitionMatrix(y, t)
-
-
-# def computeMLETransitionMatrix(state_vector: List[np.array], time_vector: np.array):
-#     assert len(state_vector) == len(time_vector)
-#     assert time_vector.ndim == 1
-#
-#     state_vector =  state_vector - np.min(state_vector)
-#
-#     n = max(state_vector) + 1
-#     mat_ = np.zeros([n, n])  # matrix if nxn dim
-#     time_ = np.zeros([n, n])
-#
-#     for (i, j, t0, t1) in zip(state_vector, state_vector[1:], time_vector, time_vector[1:]):
-#         mat_[i,j] +=1
-#         time_[i,j] += t1 - t0
-#
-#     total_time_spent_in_state = np.sum(time_, 1)
-#
-#     for i in range(n):
-#         for j in range(n):
-#             mat_[i, j] /= total_time_spent_in_state[i]
-#
-#     # populate the diagonal matrix
-#     sum_rows = np.sum(mat_, 1)
-#     for i in range(n):
-#         mat_[i, i] = sum_rows[i]
-#
-#     return mat_
+    # y, t = simulator.run_ssa(np.array([5]), 10000, parameters)
+    y, t = simulator.run_ssa(np.array([80000, 10, 10]), 500, parameters)
+    estimator = MLEstimator(model, y, t)
+    mle_matrix = estimator.computeMLETransitionMatrix()
+    fig = estimator.plotMLEstimates(mle_matrix)
+    fig.show()

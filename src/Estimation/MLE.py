7eb63c0b43ff188456c39f86c40d5c0c979ac1b7
Jan Mikelson <jan.mikelson@gmail.com>
2023-08-23 10:01:40 +0200

    extracted methods into utils for tidyness

--- src/Estimation/MLE.py
+++ src/Estimation/MLE.py
@@ -3,6 +3,7 @@ import numpy as np
 from src.Models.models import Model
 import matplotlib.pyplot as plt
 from src.Utils.GeneralUtils import getRowsCols
+from src.Estimation.utils import createPropensityPlot
 
 
 class MLEstimator:
@@ -51,36 +52,7 @@ class MLEstimator:
         for i, state in enumerate(self.__observation_vector):
             true_propensities[i] = self.__model.getPropensities(state, self.__time_vector[i])
 
-        fig = self.__createPropensityPlot(estimated_propensities, true_propensities)
-        return fig
-
-    def __createPropensityPlot(self, estimated_propensities, true_propensities):
-        nrows, ncols = getRowsCols(self.__num_reactions)
-        # Visualization
-        fig, axs = plt.subplots(nrows, ncols, figsize=(5*ncols, 5*nrows))
-        for rct_nbr in range(self.__num_reactions):
-            if nrows >1:
-                ax = axs[rct_nbr // ncols, rct_nbr % ncols]  # Select the right subplot
-            else:
-                ax = axs[rct_nbr]
-
-            # Sum true propensities for reactions with the same stoichiometry
-            sum_true_propensities = true_propensities[:, rct_nbr]
-            for k, s in enumerate(self.__stoich):
-                if k != rct_nbr and np.array_equal(s, self.__stoich[rct_nbr]):
-                    sum_true_propensities += true_propensities[:, k]
-
-            ax.scatter(sum_true_propensities, estimated_propensities[:, rct_nbr], alpha=0.5)
-            ax.set_xlabel(f'True Propensities for Reaction {rct_nbr + 1}')
-            ax.set_ylabel(f'Estimated Propensities for Reaction {rct_nbr + 1}')
-
-            min_val = min(sum_true_propensities.min(), estimated_propensities[:, rct_nbr].min())
-            max_val = max(sum_true_propensities.max(), estimated_propensities[:, rct_nbr].max())
-
-            ax.plot([min_val, max_val], [min_val, max_val], color='red', linestyle='--')
-            ax.grid(True)
-            ax.set_title(f"Comparison for Reaction {rct_nbr + 1}")
-        plt.tight_layout()
+        fig = createPropensityPlot(estimated_propensities, true_propensities)
         return fig
 
     def __extractPropensitiesFromMLEMatrix(self, mle_transition_matrix):
